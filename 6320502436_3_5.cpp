0f1d8f0dfeab20bb9953b3cd2931065ddb3d3da0
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-02-18 18:09:31 +0800

    Create 6320502436_3_5.cpp

---
+++ 6320502436_3_5.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c=2;
+    scanf("%d",&a);
+    while(1)
+    {
+        if(a%c==0)
+            {
+                printf("%d\n",c);
+                a=a/c;
+            }
+        else if(a==1)
+        {
+        	printf("%d\n",c=0);
+            break;
+        }
+        else
+        {
+        	c++;
+		}
+    }
+    
+
+    return 0;
+}
