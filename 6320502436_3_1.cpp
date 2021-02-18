8318cdf229d30b536b331b83c7e7a1d34c793a7f
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-02-18 17:13:57 +0800

    Update 6320502436_3_1.cpp

--- 6320502436_3_1.cpp
+++ 6320502436_3_1.cpp
@@ -1,16 +1,30 @@
 #include<stdio.h>
 int main ()
 {
-	int n[5][4]={},i,j,k,l;
+	int n[5][4]={},i,j,k=0,a[5]={},m=0,l;
 	for(i=0;i<=4;i++)
-		for(j=0;j<=3;J++)
+	{
+			for(j=0;j<=3;j++)
 		{
-			scanf("%d",n[i][j]);
-		}
+			scanf("%d",&n[i][j]);
+		}}
 	for(i=0;i<=4;i++)
-		for(j=0;j<=3;J++)
+	{
+		a[i]=0;
+			for(j=0;j<=3;j++)
 		{
-			printf("%d",n[i][j]);
+			a[i]=a[i]+n[i][j];
 		}
-	
+		l=a[0];
+		m=1;
+	}
+	for(i=0;i<=4;i++)
+	{
+		if(l<a[i])
+		{
+			l=a[i];
+			m=i+1;
+		}
+	}
+	printf("%d %d",m,l);
 }
