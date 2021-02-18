13399b69a742cb1d472e4e0b0eb0e0b611a6be8f
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-02-18 16:12:31 +0800

    Create 6320502436_3_1.cpp

---
+++ 6320502436_3_1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+	int n[5][4]={},i,j,k,l;
+	for(i=0;i<=4;i++)
+		for(j=0;j<=3;J++)
+		{
+			scanf("%d",n[i][j]);
+		}
+	for(i=0;i<=4;i++)
+		for(j=0;j<=3;J++)
+		{
+			printf("%d",n[i][j]);
+		}
+	
+}
