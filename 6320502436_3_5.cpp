0c2d18a7134e5c59eb09b398ab0458b50e46f8c7
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-02-18 18:38:01 +0800

    Update 6320502436_3_5.cpp

--- 6320502436_3_5.cpp
+++ 6320502436_3_5.cpp
@@ -1,26 +1,40 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c=2;
+    int a,b,c=2,g,f,h,i,j,e;
     scanf("%d",&a);
+	g=a;
+	int ak[g]={};
+    
     while(1)
     {
         if(a%c==0)
             {
-                printf("%d\n",c);
                 a=a/c;
+                ak[c]=ak[c]+1;
             }
         else if(a==1)
         {
-        	printf("%d\n",c=0);
-            break;
-        }
+        	for(e=0;e<100;e++)
+            {
+            	if(ak[e]>=3)
+            	{
+            		f=0;
+				}
+				else 
+				f=1;
+			}
+			if(f==1)
+			printf("%d is a Lucky Number",g);
+			else 
+			{
+			printf("%d is not a Lucky Number.",g);
+			break;
+        }}
         else
         {
         	c++;
 		}
     }
-    
-
     return 0;
-}
+    }
