aa10d50893bfdddbc78d7c692a4f1086c24af39b
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-02-18 17:29:05 +0800

    Update 6320502436_3_3.cpp

--- 6320502436_3_3.cpp
+++ 6320502436_3_3.cpp
@@ -5,21 +5,22 @@ int main()
     scanf("%d",&a);
     while(1)
     {
-            if(a%c==0)
+        if(a%c==0)
             {
                 printf("%d\n",c);
                 a=a/c;
             }
-            else
-            {
-                c++;
-            }
-        if(a==0)
+        else if(a==1)
         {
+        	printf("%d\n",c=0);
             break;
         }
+        else
+        {
+        	c++;
+		}
     }
-    }
+    
 
     return 0;
 }
